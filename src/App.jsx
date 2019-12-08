54c644a27044ea952b91ad73e43105f30c845362
srux <samrob.nz@gmail.com>
2019-12-08 21:52:21 +1300

    4 cols added plus transitions

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 import Iframe from 'react-iframe';
 import Tilt from 'react-tilt';
 import { Scrollbars } from 'react-custom-scrollbars';
+import { IoMdClose }  from "react-icons/io";
+
 
 
 class App extends Component {
@@ -20,7 +22,36 @@ class App extends Component {
 
 
 
+resetClick = (e) => {
+  setTimeout(() => {
+    this.setState({
+      colExpand:' transitisonNone',
+      wrapState:' transitionNone',
+      lock:'noPointer transitionNone',
+      wrapState:'',
+    })
+}, 100)
+setTimeout(() => {
+  this.setState({
+    cover:'',
+  })
+}, 1000)
+
+    // window.location.reload(false);
+ 
+}
+
 colClick = (e) => {
+  this.setState({
+    colExpand:e.target.id,
+    wrapState:'',
+    cover:'active',
+    lock:'',
+  })
+  console.log(e.target.id);
+}
+
+colClickTwo = (e) => {
   this.setState({
     colExpand:e.target.id,
     wrapState:'open',
@@ -28,7 +59,6 @@ colClick = (e) => {
     lock:'',
   })
   console.log(e.target.id);
-
 }
 
 render () {
@@ -36,21 +66,24 @@ render () {
 
   return (
     <div className="App">
-      <div className={"cover "+cover}></div>
+      <div className={"close"+cover} onClick={this.resetClick}><IoMdClose /></div>
+      <div className={"cover "+cover}>
+        
+      </div>
       <div className={"wrap "+wrapState}>
-        <div className="container">
+        <div className={"container "+wrapState}>
 
           <div id={"oneExpand set"} onClick={e => this.colClick(e)} className={"col col1"+colExpand}>
           <div className={"mobContainer "+lock}>
                     <Tilt
-                                      className="webWorkMobileTilt"
+                                      className={"webWorkMobileTilt "+lock}
                                       options={{
                                                       perspective:2000,
                                                       max:25,
               
                                                       scale: 1.1,
                                                       axis:'x',
-                                                      speed:4000,
+                                                      speed:5000,
                                                       easing:"cubic-bezier(.03,.98,.52,.99)",
                                                   }}>
               
@@ -84,7 +117,7 @@ render () {
                                                       perspective:2000,
                                                       max:25,
               
-                                                      scale: 1.1,
+                                                      scale: 1.05,
                                                       axis:'x',
                                                       speed:4000,
                                                       easing:"cubic-bezier(.03,.98,.52,.99)",
@@ -113,13 +146,120 @@ render () {
           </div>
         </div>
         <div className="container">
-          <div className={"col col3"+colExpand}></div>
-          <div className={"col col4"+colExpand}></div>
+          <div className={"col col3"+colExpand}> <div id={"threeExpand set"}  onClick={e => this.colClickTwo(e)} className={"col col3"+colExpand}>
+
+<div className={"mobContainer "+lock}>
+        <Tilt
+                          className="webWorkMobileTilt"
+                          options={{
+                                          perspective:2000,
+                                          max:25,
+  
+                                          scale: 1.1,
+                                          axis:'x',
+                                          speed:4000,
+                                          easing:"cubic-bezier(.03,.98,.52,.99)",
+                                      }}>
+  
+                          <div
+                              className={"webWorkMobile Tilt-inner "}
+                              onMouseEnter={this.handleMobVerticalLock}
+                              onMouseLeave={this.handleMobVerticalActive}>
+                              <Scrollbars
+                                  className="webWorkInner"
+                                  autoHideTimeout={1000}
+                                  autoHideDuration={200}>
+                             
+                                          <div className="webImage inapp-m"><Iframe url="https://new.wearenomad.nz/"
+                                            
+                                                      id="inapp-mobile"
+                                                      className="inapp-mobile"
+                                                      display="initial"
+                                                      position="relative"/></div>
+                                      
+                              </Scrollbars>
+                          </div>
+                      </Tilt>
+                      </div>
+</div>
+</div>
+<div className={"col col4"+colExpand}> <div id={"fourExpand set"}  onClick={e => this.colClickTwo(e)} className={"col col4"+colExpand}>
+
+<div className={"mobContainer "+lock}>
+
+        <Tilt
+                          className="webWorkMobileTilt"
+                          options={{
+                                          perspective:2000,
+                                          max:25,
+  
+                                          scale: 1.1,
+                                          axis:'x',
+                                          speed:4000,
+                                          easing:"cubic-bezier(.03,.98,.52,.99)",
+                                      }}>
+  
+                          <div
+                              className={"webWorkMobile Tilt-inner "}
+                              onMouseEnter={this.handleMobVerticalLock}
+                              onMouseLeave={this.handleMobVerticalActive}>
+                              <Scrollbars
+                                  className="webWorkInner"
+                                  autoHideTimeout={1000}
+                                  autoHideDuration={200}>
+                             
+                                          <div className="webImage inapp-m">
+                                            <Iframe url="https://new.wearenomad.nz/"
+                                            
+                                                      id="inapp-mobile"
+                                                      className="inapp-mobile"
+                                                      display="initial"
+                                                      position="relative"/></div>
+                                      
+                              </Scrollbars>
+                          </div>
+                      </Tilt>
+                      </div>
+                      <Tilt
+                        className={"webWorkTilt "+lock}
+                        options={{
+                                        perspective:2000,
+                                        max:25,
+                                        reverse:true,
+                                        scale: 1.1,
+                                        axis:'x',
+                                        speed:4000,
+                                        easing:"cubic-bezier(.03,.98,.52,.99)",
+                                    }}>
+                        <div
+                            className={"webWork "}
+                            onMouseEnter={this.handleVerticalLock}
+                            onMouseLeave={this.handleVerticalActive}>
+                            <Scrollbars
+                                className="webWorkInner"
+                                autoHideTimeout={1000}
+                                autoHideDuration={200}>
+                               
+                                    <div className="webImage mj">
+                                    <Iframe url="https://new.wearenomad.nz/"
+                                            
+                                            id="inapp-mobile"
+                                            className="inapp-mobile"
+                                            display="initial"
+                                            position="relative"/>
+                                    </div>
+                                
+                            </Scrollbars>
+                        </div>
+
+                    </Tilt>
+</div>
+</div>
         </div>
-        <div className="container">
+        {/* <div className="container">
           <div className={"col col5"+colExpand}></div>
           <div className={"col col6"+colExpand}></div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
